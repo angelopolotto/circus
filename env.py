074452b765e7421998b784e27e0012c8b2766a8c
Ângelo Polotto <angelopolotto@yahoo.com.br>
2018-08-26 17:43:15 -0300

    env adjustments

--- env.py
+++ env.py
@@ -2,13 +2,13 @@ import os
 
 PORT = int(os.environ.get('PORT', 5000))
 # use '0.0.0.0' to ensure your REST API is reachable from all your
-# network (and not only your computer).
-HOST = os.environ.get('HOST', '127.0.0.1')
+# network (and not only your computer). For so your computer only, set 127.0.0.1
+HOST = os.environ.get('HOST', '0.0.0.0')
 
-URL_DB = 'srv-captain--circusdb'
+URL_DB = 'localhost'
 PORT_DB = 27017
 USER_DB = 'admin'
-PASS_DB = '35VaBRVG36423kYVqFDTnHVX'
+PASS_DB = '123456879'
 NAME_DB = 'admin'
 
 MONGO_URI = os.environ.get('MONGODB_URI',
